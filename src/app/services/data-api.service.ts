c93c267f04a5df530108924ab63f5be137441c2e
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-04-18 10:03:19 -0500

    Cambio en dataApiService_ts
    - Se inicializó la propiedad id de libro para el if que diferencia new / update.

--- src/app/services/data-api.service.ts
+++ src/app/services/data-api.service.ts
@@ -17,7 +17,9 @@ export class DataApiService {
   private books: Observable<BookInterface[]>;
   private bookDoc: AngularFirestoreDocument<BookInterface>;
   private book: Observable<BookInterface>;
-  public selectedBook: BookInterface = {};
+  public selectedBook: BookInterface = {
+    id: null
+  };
 
   getAllBooks(){
   	return this.books = this.booksCollection.snapshotChanges()
