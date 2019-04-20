430e10c16988af3049329bdb436d2a4f1bb67279
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2019-04-20 00:48:41 -0500

    Cambios para puesta en producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+  	apiKey: "AIzaSyBmp2zQCNZZ-vo6q30a7SI4ifcH5FJX8Cc",
+    authDomain: "bookstore-e4647.firebaseapp.com",
+    databaseURL: "https://bookstore-e4647.firebaseio.com",
+    projectId: "bookstore-e4647",
+    storageBucket: "bookstore-e4647.appspot.com",
+    messagingSenderId: "139313675064"
+  }
 };
